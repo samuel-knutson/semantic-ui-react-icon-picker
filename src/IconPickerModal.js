79b1da214e22370a4487a662b7fd387eeb1253c4
Sam Knutson <samuel.r.knutson@gmail.com>
2020-06-28 15:48:16 -0400

    Implement infinite scroll for IconPickerModal to improve performance

--- src/IconPickerModal.js
+++ src/IconPickerModal.js
@@ -1,59 +1,137 @@
-import React, { useState } from 'react';
-import { Modal, Button, Grid, Popup, Input } from 'semantic-ui-react';
-import { icons } from './icons';
+import React from 'react';
+import { Modal, Button, Popup, Input } from 'semantic-ui-react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { icons as allIcons } from './icons';
 
-const IconPickerModal = ({ value, onChange }) => {
-  const [open, setOpen] = useState(false);
-  const [query, setQuery] = useState('');
+const SCROLLABLE_CONTAINER_HEIGHT = 400;
+const INITIAL_LIMIT = 64;
+const PAGE_SIZE = 32;
 
-  return (
-    <Modal
-      trigger={
-        <Button
-          icon={value || undefined}
-          content={value ? undefined : 'Select Icon'}
-          onClick={() => setOpen(true)}
+class IconPickerModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      filteredIcons: allIcons,
+      limit: INITIAL_LIMIT
+    };
+  }
+
+  onSearch = (e) => {
+    const { limit } = this.state;
+    const query = e.target.value;
+    const matchingIcons = allIcons.filter((icon) => icon.match(query));
+    const adjustedLimit = Math.min(
+      Math.max(INITIAL_LIMIT, limit),
+      matchingIcons.length
+    );
+
+    this.setState({
+      filteredIcons: matchingIcons,
+      limit: adjustedLimit
+    });
+  };
+
+  fetchMore = () => {
+    const { filteredIcons, limit } = this.state;
+    if (limit > filteredIcons.length) {
+      return;
+    }
+
+    this.setState({
+      limit: Math.min(limit + PAGE_SIZE, filteredIcons.length)
+    });
+  };
+
+  close = () => {
+    this.setState({
+      open: false,
+      filteredIcons: allIcons,
+      limit: INITIAL_LIMIT
+    });
+  };
+
+  renderTrigger() {
+    const { value } = this.props;
+    return (
+      <Button
+        icon={value || undefined}
+        content={value ? undefined : 'Select Icon'}
+        onClick={() => this.setState({ open: true })}
+      />
+    );
+  }
+
+  renderIcons() {
+    const { onChange } = this.props;
+    const { filteredIcons, limit } = this.state;
+
+    return filteredIcons.slice(0, limit).map((icon) => (
+      <div key={icon} className='two wide column'>
+        <Popup
+          content={icon}
+          trigger={
+            <Button
+              icon={icon}
+              onClick={() => {
+                onChange(icon);
+                this.close();
+              }}
+            />
+          }
         />
-      }
-      closeIcon
-      open={open}
-      onClose={() => setOpen(false)}
-    >
-      <Modal.Header>Select an Icon</Modal.Header>
-      <Modal.Content scrolling>
-        <Grid columns={8}>
-          {icons
-            .filter((icon) => icon.match(query))
-            .map((icon) => (
-              <Grid.Column key={icon}>
-                <Popup
-                  content={icon}
-                  trigger={
-                    <Button
-                      icon={icon}
-                      onClick={() => {
-                        onChange(icon);
-                        setOpen(false);
-                      }}
-                    />
-                  }
-                />
-              </Grid.Column>
-            ))}
-        </Grid>
-      </Modal.Content>
-      <Modal.Actions>
+      </div>
+    ));
+  }
+
+  renderContent() {
+    const { filteredIcons, limit } = this.state;
+    return (
+      <InfiniteScroll
+        className='ui grid'
+        style={{ alignContent: 'flex-start' }}
+        dataLength={limit}
+        next={this.fetchMore}
+        hasMore={limit < filteredIcons.length}
+        initialScrollY={0}
+        height={SCROLLABLE_CONTAINER_HEIGHT}
+      >
+        {this.renderIcons()}
+      </InfiniteScroll>
+    );
+  }
+
+  renderActionBar() {
+    return (
+      <React.Fragment>
         <Input
+          autoFocus
           icon='search'
           placeholder='Search...'
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
+          onChange={this.onSearch}
           style={{ float: 'left' }}
         />
-        <Button content='Cancel' onClick={() => setOpen(false)} />
-      </Modal.Actions>
-    </Modal>
-  );
-};
+        <Button content='Cancel' onClick={this.close} />
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    const { open } = this.state;
+
+    return (
+      <Modal
+        trigger={this.renderTrigger()}
+        closeIcon
+        open={open}
+        onClose={this.close}
+      >
+        <Modal.Header>Select an Icon</Modal.Header>
+        <Modal.Content>{this.renderContent()}</Modal.Content>
+        <Modal.Actions>{this.renderActionBar()}</Modal.Actions>
+      </Modal>
+    );
+  }
+}
 
 export default IconPickerModal;
